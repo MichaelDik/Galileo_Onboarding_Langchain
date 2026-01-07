648f17b11974606340e2e8c5639e44775d975873
MichaelDik <mwdik@comcast.net>
2026-01-07 09:05:00 -0500

    working on session unity

--- agent/main.py
+++ agent/main.py
@@ -1,30 +1,28 @@
 #https://v2docs.galileo.ai/cookbooks/use-cases/agent-langchain | Following this video
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent, Tool
+from langchain.agents import initialize_agent
 from langchain.agents.agent_types import AgentType
 from langchain_openai import ChatOpenAI
-from langchain.tools import tool
 from galileo import galileo_context
 from galileo.handlers.langchain import GalileoCallback
-import os
+from galileo import GalileoLogger
+
+from tools.stock_advice import stock_advice_tool
 
 
 load_dotenv()
 
-#Define a Tool 
-@tool
-def greet(name: str) -> str: 
-    """Say Hello to someone."""
-    return f"Hello, {name}!"
+context = galileo_context.init(project="Financial Advisor Agent", log_stream="dev_main_ls")
 
-with galileo_context(project="Financial Advisor Agent", log_stream="dev_main_ls"):
-    agent = initialize_agent(
-        tools=[greet],
-        llm=ChatOpenAI(model="gpt-4", temperature=0.7, callbacks=[GalileoCallback()]),
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
-    )
+logger= GalileoLogger()
+
+agent = initialize_agent(
+    tools=[stock_advice_tool],
+    llm=ChatOpenAI(model="gpt-4", temperature=0.7, callbacks=[GalileoCallback(galileo_logger=logger)]),
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+)
 
 if __name__ == "__main__":
-    result = agent.invoke({"input": "Say hello to Michael"})
-    print(f"\nAgent Response:\n{result}")
+    result = agent.invoke({"input": "Get stock advice for AAPL and explain the recommendation."})
+    print(f"\nAgent Response:\n{result['output']}")
